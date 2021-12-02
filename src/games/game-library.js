b511c72047ac2a44cd1d75fbea9ea42bbe986ef2
Ladadori <vladagolunova@gmail.com>
2021-12-02 11:42:14 +1000

    fix: Remove unused readlineSync import

--- src/games/game-library.js
+++ src/games/game-library.js
@@ -1,5 +1,3 @@
-import readlineSync from 'readline-sync';
-
 export const log = console.log;
 export const isAnswerCorrect = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
 export const printQuestion = (question) => log(`Question: ${question}`);
