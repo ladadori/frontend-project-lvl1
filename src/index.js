f7280fada0823901aba82f41ef1aa3d46785ceca
Ladadori <vladagolunova@gmail.com>
2021-12-02 14:25:01 +1000

    fix: Move game logic to index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ export const { log } = console;
 export const isAnswerCorrect = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
 export const printQuestion = (question) => log(`Question: ${question}`);
 export const getUserAnswer = () => readlineSync.question('Your answer: ');
-
 export const getRandomInteger = (integerLimit) => Math.floor(Math.random() * integerLimit);
+export const printSorry = (name, userAnswer, correctAnswer) => {
+  log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+  log(`Let's try again, ${name}`);
+};
