885f7ce291a3473d409f9e9fa3ec566fd5a989f7
Ladadori <vladagolunova@gmail.com>
2021-12-02 11:35:23 +1000

    fix: Remove printGreeting() which saves player's name
    I need player's name in the scope of my game function. Before that it was hidden in the scope of greeting function and could be used only once. Now I have it in game scope and it can be used again.

--- src/games/game-library.js
+++ src/games/game-library.js
@@ -9,9 +9,3 @@ export const getUserAnswer = () => readlineSync.question('Your answer: ');
 export const getRandomInteger = (integerLimit) => {
   Math.floor(Math.random() * integerLimit);
 };
-
-export const printGreeting = () => {
-  log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  log(`Hello, ${name}!`);
-};
