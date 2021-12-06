f2f993d3fa3177e413ce5d72f7ed58b789ddab55
Ladadori <vladagolunova@gmail.com>
2021-12-06 20:32:55 +1000

    refact: Pass functions to game engine function

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import askName from './cli.js'
 
 export const { log } = console; // мне так удобнее. если это не криминал, я бы не удаляла
 
-export const getRandomInteger = (max, min = 0) => {
+export const getRandomInteger = (max, min = 1) => {
   const minNum = Math.ceil(min);
   const maxNum = Math.floor(max);
   return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
 };
 
-export const runGameEngine = (gameRule, question, correctAnswer) => {
+export const runGameEngine = (gameRule, getQuestion, getCorrectAnswer) => {
 
   const maxRoundCount = 3;
   let roundCount = 0
@@ -19,13 +19,18 @@ export const runGameEngine = (gameRule, question, correctAnswer) => {
   log(gameRule);
 
   for (; roundCount < maxRoundCount; roundCount += 1) {
+    
+    const question = getQuestion();
+    log(`Question: ${question}`);
 
-    const userAnswer = readlineSync.question(`${question}`);
+    const userAnswer = readlineSync.question(`Your answer: `);
+    const correctAnswer = getCorrectAnswer(question);
+
     const userVictory = userAnswer === correctAnswer;
 
     if (userVictory === false) {
       log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-      log(sorry);
+      log(`Let's try again, ${name}!`);
       break;
     }
 
