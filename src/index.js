8543c98b0bb83f72c591b7c0ceac57bb93f35faa
Ladadori <vladagolunova@gmail.com>
2021-12-06 19:45:54 +1000

    refactor: Remove everything but game engine

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 import readlineSync from 'readline-sync';
+import askName from './cli.js'
 
-export const { log } = console; // мне так удобнее. если это не криминал, я бы не исправляла
-
-export const printGameRules = (gameName) => {
-  switch (gameName) {
-    case 'brainEven':
-      log('Answer "yes" if the number is even, otherwise answer "no".');
-      break;
-    case 'brainCalc':
-      log('What is the result of the expression?');
-      break;
-    case 'brainGcd':
-      log('Find the greatest common divisor of given numbers.');
-      break;
-    case 'brainProgression':
-      log('What number is missing in the progression?');
-      break;
-    case 'brainPrime':
-      log('Answer "yes" if given number is prime. Otherwise answer "no".');
-      break;
-    default: // это тут из-за правила eslint default-case
-  }
-};
-
-export const isAnswerCorrect = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
-
-export const printQuestion = (question, optionalQuestion = '') => log(`Question: ${question} ${optionalQuestion}`);
-export const getUserAnswer = () => readlineSync.question('Your answer: ');
+export const { log } = console; // мне так удобнее. если это не криминал, я бы не удаляла
 
 export const getRandomInteger = (max, min = 0) => {
   const minNum = Math.ceil(min);
@@ -34,79 +9,30 @@ export const getRandomInteger = (max, min = 0) => {
   return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
 };
 
-export const getRandomExpression = (IntegerLimit) => {
-  const leftOperand = getRandomInteger(IntegerLimit);
-  const rightOperand = getRandomInteger(IntegerLimit);
-  const operators = ['+', '-', '*'];
-  const getRandomOperator = () => {
-    const indexOfOperator = getRandomInteger(3);
-    const operator = operators[indexOfOperator];
-    return operator;
-  };
-  const operator = getRandomOperator();
-  const stringExpression = `${leftOperand} ${operator} ${rightOperand}`;
-  const expressionCollection = [stringExpression, leftOperand, operator, rightOperand];
-  return expressionCollection;
-};
+export const runGameEngine = (gameRule, question, correctAnswer) => {
+  
+  const maxRoundCount = 3;
+  let roundCount = 0
 
-export const getCalculation = (leftOperand, operator, rightOperand) => {
-  let answer;
-  switch (operator) {
-    case '-':
-      return leftOperand - rightOperand;
-    case '+':
-      return leftOperand - rightOperand;
-    case '*':
-      return leftOperand - rightOperand;
-    default:
-  }
-  return 'fuck consistent-return rule';
-};
+  log('Welcome to the Brain Games!');
+  const name = askName();
+  log(gameRule);
 
-//  vvv Слизала с инернета, сама не смогла за полчаса написать.
-export const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+  for (; roundCount < maxRoundCount; roundCount += 1) {
 
-const getProgression = (lengthLimit) => {
-  const progressionStep = getRandomInteger(5, 2);
-  const firstProgressionNumber = getRandomInteger(10);
-  const progression = [firstProgressionNumber];
-  let lastAddedNumberIndex = 0;
-  while (progression.length < lengthLimit) {
-    const newProgressionNumber = progression[lastAddedNumberIndex] + progressionStep;
-    progression.push(newProgressionNumber);
-    lastAddedNumberIndex += 1;
-  }
-  return progression;
-};
+    const userAnswer = readlineSync.question(`${question}`);
+    const userVictory = userAnswer === correctAnswer;
 
-export const getProgressionRiddle = (lengthLimit) => {
-  const progression = getProgression(lengthLimit);
-  const progressionLength = progression.length;
-  const blankIndex = getRandomInteger(progressionLength);
-  const hiddenNumber = progression[blankIndex];
-  const progressionWithBlank = progression.slice(0);
-  progressionWithBlank[blankIndex] = '..';
-  const stringProgressionWithBlank = progressionWithBlank.join(' ');
-  return [progressionWithBlank, stringProgressionWithBlank, hiddenNumber];
-};
-
-export const isPrime = (integer) => { // решение со стак оверфлоу,
-  const squareOfInteger = Math.sqrt(integer); // ибо список простых чисел копировался
-  for (let i = 2; i <= squareOfInteger; i += 1) { // с поехавшим форматированием.
-    if (integer % i === 0) {
-      return false;
+    if (userVictory === false) {
+      log(sorry);
+      break;
     }
-  }
-  return integer > 1;
-};
 
-export const getRoundScore = (userAnswer, correctAnswer, name) => {
-  const userVictory = isAnswerCorrect(userAnswer, correctAnswer);
-  if (userVictory === true) {
     log('Correct!');
-    return 1;
+
   }
-  log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-  log(`Let's try again, ${name}!`);
-  return 0;
+
+  if (roundCount === 3) {
+    log(`Congratulations, ${name}`);
+  }
 };
