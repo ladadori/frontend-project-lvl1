043ca494b1d7ef3fedcd6c246e6943ad57735631
Ladadori <vladagolunova@gmail.com>
2021-12-15 10:25:26 +1000

    refact: Move number generator to separate module

---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const getRandomInteger = (min = 1, max = 100) => {
+    const minNum = Math.ceil(min);
+    const maxNum = Math.floor(max);
+    return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
+  };
