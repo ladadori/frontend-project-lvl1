2c08b7f3b7425860798196cdf958d4f566835cde
Ladadori <vladagolunova@gmail.com>
2021-12-17 14:30:11 +1000

    refact: Hide expression generator and calculator to gamedata generator

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -5,34 +5,35 @@ import generateRandomNumber from '../utils.js';
 const gameRule = 'What is the result of the expression?';
 const maxNum = 10;
 
-const generateRandomExpression = (NumberLimit) => {
-  const leftOperand = generateRandomNumber(1, NumberLimit);
-  const rightOperand = generateRandomNumber(1, NumberLimit);
-  const operators = ['+', '-', '*'];
-  const countOfOperators = operators.length;
-  const generateRandomOperator = () => {
-    const indexOfOperator = generateRandomNumber(1, countOfOperators);
-    const operator = operators[indexOfOperator];
-    return operator;
-  };
-  const operator = generateRandomOperator();
-  const operandsAndOperator = [leftOperand, operator, rightOperand];
-  return operandsAndOperator;
-};
-
-const calculate = (operandsAndOperator) => {
-  const [leftOperand, operator, rightOperand] = operandsAndOperator;
-  switch (operator) {
-    case '+':
-      return leftOperand + rightOperand;
-    case '*':
-      return leftOperand * rightOperand;
-    default:
-  }
-  return leftOperand - rightOperand;
-};
-
 const generateGameData = () => {
+
+  const generateRandomExpression = (NumberLimit) => {
+    const leftOperand = generateRandomNumber(1, NumberLimit);
+    const rightOperand = generateRandomNumber(1, NumberLimit);
+    const operators = ['+', '-', '*'];
+    const countOfOperators = operators.length;
+    const generateRandomOperator = () => {
+      const indexOfOperator = generateRandomNumber(1, countOfOperators);
+      const operator = operators[indexOfOperator];
+      return operator;
+    };
+    const operator = generateRandomOperator();
+    const operandsAndOperator = [leftOperand, operator, rightOperand];
+    return operandsAndOperator;
+  };
+  
+  const calculate = (operandsAndOperator) => {
+    const [leftOperand, operator, rightOperand] = operandsAndOperator;
+    switch (operator) {
+      case '+':
+        return leftOperand + rightOperand;
+      case '*':
+        return leftOperand * rightOperand;
+      default:
+    }
+    return leftOperand - rightOperand;
+  };
+
   const operandsAndOperator = generateRandomExpression(maxNum);
   const [leftOperand, operator, rightOperand] = operandsAndOperator;
   const correctAnswer = calculate(operandsAndOperator).toString();
