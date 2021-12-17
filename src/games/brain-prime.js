c61bc43058424a8114af35cc4f40eafcc3aa559f
Ladadori <vladagolunova@gmail.com>
2021-12-17 15:33:44 +1000

    fix: Change guard exp

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -6,15 +6,13 @@ const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 const maxNum = 10;
 const isPrime = (number) => {
   if (number > 1) {
-    return true;
-  }
-  const squareOfNumber = Math.sqrt(number);
-  for (let i = 2; i <= squareOfNumber; i += 1) {
-    if (number % i === 0) {
-      return false;
+    const squareOfNumber = Math.sqrt(number);
+    for (let i = 2; i <= squareOfNumber; i += 1) {
+      if (number % i === 0) {
+        return false;
+      }
     }
   }
-
   return true;
 };
 
