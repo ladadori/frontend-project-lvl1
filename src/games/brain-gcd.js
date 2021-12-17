d47a7ff7c11404cf0130e71742ea7153bb7199de
Ladadori <vladagolunova@gmail.com>
2021-12-17 14:31:52 +1000

    refact: Delete unnessesary funcs and shorten them to 3 lines

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -7,15 +7,9 @@ const getGCD = (a, b) => (b === 0 ? a : getGCD(b, a % b));
 const gameRule = 'Find the greatest common divisor of given numbers.';
 const maxNum = 20;
 
-const generateNumPair = () => {
-  const firstNum = generateRandomNumber(1, maxNum);
-  const secondNum = generateRandomNumber(1, maxNum);
-  const numPair = [firstNum, secondNum];
-  return numPair;
-};
-
 const generateGameData = () => {
-  const [a, b] = generateNumPair();
+  const a = generateRandomNumber(1, maxNum);
+  const b = generateRandomNumber(1, maxNum);
   const question = `${a} ${b}`;
   const correctAnswer = getGCD(a, b).toString();
   return [question, correctAnswer];
